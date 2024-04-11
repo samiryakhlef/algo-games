0f6fc16a4ed0e53006193502d744f6c0730553ce
samir <samir.yakhlef30@gmail.com>
2024-04-11 12:27:10 +0200

    issue resolve

--- Console Games/obj/Debug/net8.0/Console Games.AssemblyInfo.cs
+++ Console Games/obj/Debug/net8.0/Console Games.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Console Games")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f95866a11c8382913a5606337d28205bdb19224")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93589b7c61805d864c378c91f45136d0628bb1f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Console Games")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Console Games")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
